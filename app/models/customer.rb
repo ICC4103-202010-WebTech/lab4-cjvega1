1ca2918cb1792b802383e8aeb1367eca736129cc
cjvega1 <43618351+cjvega1@users.noreply.github.com>
2020-04-11 13:31:11 -0400

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :lastname, presence: true
 end
